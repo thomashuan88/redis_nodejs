f8dee01efe417794f7bb450859cd3cbf1d281d2d
thomas huan <thomashuan88@gmail.com>
2016-08-03 02:10:22 +0800

    learn socket.io

--- learn_socketio/app.js
+++ learn_socketio/app.js
@@ -2,11 +2,12 @@ var io = require('socket.io').listen(4000);
 
 io.sockets.on('connection', function(socket){
     socket.on('join', function(data) {
-        io.sockets.emit('userJoined', data);
+        socket.emit('userJoined', data);
         socket.username = data.username;        
     });
-    socket.on('ping', function(data, done) {
-        io.sockets.emit('ping', {username: socket.username});
+    socket.on('toserver', function(data, done) {
+        console.log(data);
+        socket.emit('fromserver', {username: socket.username});
         done('ack');
     })
 

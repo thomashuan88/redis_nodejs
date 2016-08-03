80a4b11433eeb5ad30d3be69a58c294ad074d83c
thomas huan <thomashuan88@gmail.com>
2016-08-03 10:54:25 +0800

    broadcast

--- learn_socketio/app.js
+++ learn_socketio/app.js
@@ -2,12 +2,11 @@ var io = require('socket.io').listen(4000);
 
 io.sockets.on('connection', function(socket){
     socket.on('join', function(data) {
-        socket.emit('userJoined', data);
+        socket.broadcast.emit('userJoined', data);
         socket.username = data.username;        
     });
     socket.on('toserver', function(data, done) {
-        console.log(data);
-        socket.emit('fromserver', {username: socket.username});
+        socket.broadcast.emit('fromserver', {username: socket.username});
         done('ack');
     })
 

ad55e80b014d18500f6bb36859adc173b9b0ce45
thomas huan <thomashuan88@gmail.com>
2016-11-09 10:05:58 +0800

    networking

--- networking/tcpserver.js
+++ networking/tcpserver.js
@@ -25,16 +25,20 @@ var tcpServer = net.createServer(function(socket) {
         console.log('something wrong happened here');
         // socket.end('socket can send some more data but it will ended');
         // socket.destroy();
-    })
+    }) 
 });
 
 tcpServer.maxConnections = 10; 
 
 setTimeout(function() {
     tcpServer.close(function() {
-        console.log('server closed');
+        console.log('server closed'); 
     });
 }, 60000);
+
+tcpServer.on('close', function() {
+    console.log('second close event handler');
+});
 
 tcpServer.listen(function() {
     var port = tcpServer.address().port
